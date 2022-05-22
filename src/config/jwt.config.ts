a5f14c2c2990b8336a7134e2447cd1a90ed5c7b8
Igor Samartsev <igorsamartsev@gmail.com>
2022-05-22 23:46:50 +0200

    initial server config

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,11 @@
+import { ConfigService } from '@nestjs/config'
+import { JwtModuleOptions } from '@nestjs/jwt'
+import { TypegooseModuleOptions } from 'nestjs-typegoose'
+
+export const getJwtConfig = async (
+  configService: ConfigService,
+): Promise<JwtModuleOptions> => {
+  return {
+    secret: configService.get('JWT_SECRET'),
+  }
+}
