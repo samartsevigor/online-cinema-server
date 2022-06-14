c6633bbb24651c88211f7d75b1c26346fc9ab4e6
samartsevigor <igorsamartsev@gmail.com>
2022-06-15 01:21:50 +0200

    drop garbage

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -8,10 +8,8 @@ export class FileService {
   async saveFiles(files: Express.Multer.File[], folder: string = 'default'): Promise<FileResponse[]> {
     const uploadFolder = `${path}/uploads/${folder}`
     await ensureDir(uploadFolder)
-    console.log(5555)
     const res: FileResponse[] = await Promise.all(
       files.map(async (file) => {
-        console.log(file, 6666)
         await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer)
         return {
           url: `/uploads/${folder}/${file.originalname}`,
