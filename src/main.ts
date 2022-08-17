efac6e76a3c91e448276d8e5a6e83afe04bdf550
samartsevigor <igorsamartsev@gmail.com>
2022-08-17 18:09:28 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import { AppModule } from './app.module';
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableCors();
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+  app.enableCors({ origin: '*' })
 
   const config = new DocumentBuilder()
     .setTitle('Cinema swagger')
     .setVersion('1.0')
     .setBasePath('api')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
-  await app.listen(parseInt(process.env.PORT) || 3000);
+  await app.listen(parseInt(process.env.PORT) || 3000)
 }
-bootstrap();
+bootstrap()
